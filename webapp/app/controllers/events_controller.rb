1886bd54c0907bbcc5f31325442816d1d62bde99
sic2 <sic2@st-andrews.ac.uk>
2016-07-05 19:38:06 +0100

    showing upcoming, future and old events

--- webapp/app/controllers/events_controller.rb
+++ webapp/app/controllers/events_controller.rb
@@ -1,9 +1,13 @@
+require 'date'
+
 class EventsController < ApplicationController
 
 def index
-    #@future = Event.where().limit(100).order('starts_at desc')
-    @upcoming = Event.where().limit(100).order('starts_at desc')
+	three_weeks = Time.now + (3*7*24*60*60) 
+    @future = Event.where("starts_at >= ?", three_weeks).limit(100).order('starts_at desc')
+    @upcoming = Event.where(:starts_at => Time.zone.now.beginning_of_day..three_weeks).limit(100).order('starts_at desc')
     @old = Event.where("starts_at < ?", Time.zone.now.beginning_of_day).limit(100).order('starts_at desc')
   end
 
 end
+
