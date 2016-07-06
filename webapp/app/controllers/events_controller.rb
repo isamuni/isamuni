efe3374d125d91c8e76724d9dbd1b3254ba965e2
sic2 <sic2@st-andrews.ac.uk>
2016-07-06 20:37:03 +0100

    render events to json

--- webapp/app/controllers/events_controller.rb
+++ webapp/app/controllers/events_controller.rb
@@ -1,10 +1,16 @@
 class EventsController < ApplicationController
 
   def index
-	three_weeks = Time.now + (3*7*24*60*60) 
+	  three_weeks = Time.now + (3*7*24*60*60) 
     @future = Event.where("starts_at >= ?", three_weeks).limit(100).order('starts_at desc')
     @upcoming = Event.where(:starts_at => Time.zone.now.beginning_of_day..three_weeks).limit(100).order('starts_at desc')
     @old = Event.where("starts_at < ?", Time.zone.now.beginning_of_day).limit(100).order('starts_at desc')
+
+    respond_to do |format|
+      events = {'upcoming' => @upcoming, 'future' => @future, 'old' => @old}
+      format.html { render :index }
+      format.json { render json: events }
+    end
   end
 
   def typeahead
