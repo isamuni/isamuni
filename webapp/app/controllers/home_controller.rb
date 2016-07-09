c91340c9a129242be00aa923d5d5f9523198d7e4
sic2 <sic2@st-andrews.ac.uk>
2016-07-09 21:18:35 +0100

    removed useless comment

--- webapp/app/controllers/home_controller.rb
+++ webapp/app/controllers/home_controller.rb
@@ -6,7 +6,6 @@ class HomeController < ApplicationController
 
   end
 
-
   def data
     posts = Post.select("date(created_at) as created_at").group("date(created_at)").order('created_at desc').distinct.count(:uid)
 
@@ -40,25 +39,3 @@ class HomeController < ApplicationController
   end
 
 end
-
-
-# {
-#   cols: [{id: 'A', label: 'NEW A', type: 'string'},
-#          {id: 'B', label: 'B-label', type: 'number'},
-#          {id: 'C', label: 'C-label', type: 'date'}
-#   ],
-#   rows: [{c:[{v: 'a'},
-#              {v: 1.0, f: 'One'},
-#              {v: new Date(2008, 1, 28, 0, 31, 26), f: '2/28/08 12:31 AM'}
-#         ]},
-#          {c:[{v: 'b'},
-#              {v: 2.0, f: 'Two'},
-#              {v: new Date(2008, 2, 30, 0, 31, 26), f: '3/30/08 12:31 AM'}
-#         ]},
-#          {c:[{v: 'c'},
-#              {v: 3.0, f: 'Three'},
-#              {v: new Date(2008, 3, 30, 0, 31, 26), f: '4/30/08 12:31 AM'}
-#         ]}
-#   ],
-#   p: {foo: 'hello', bar: 'world!'}
-# }
