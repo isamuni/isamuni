6d9e2c8ef492252cce07a8fef2472d674ef3797a
Gabriele Viglianisi <vigliag@gmail.com>
2016-07-10 00:40:15 +0200

    added Dockerfile and docker-compose.yml

--- webapp/config/environments/development.rb
+++ webapp/config/environments/development.rb
@@ -38,4 +38,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  config.web_console.whitelisted_ips = '172.18.0.0/16'
 end
