0352d3278631d281c9fa6004e4cc54c587e6d3d5
sic2 <sic2@st-andrews.ac.uk>
2016-07-16 22:57:28 +0100

    better events modal + fixed typeahead for events

--- webapp/app/controllers/events_controller.rb
+++ webapp/app/controllers/events_controller.rb
@@ -1,17 +1,23 @@
 class EventsController < ApplicationController
 
   def index
-	  three_weeks = Time.now + (3*7*24*60*60) 
-    @future = Event.where("starts_at >= ?", three_weeks).limit(100).order('starts_at desc')
-    @upcoming = Event.where(:starts_at => Time.zone.now.beginning_of_day..three_weeks).limit(100).order('starts_at desc')
-    @old = Event.where("starts_at < ?", Time.zone.now.beginning_of_day).limit(78).order('starts_at desc')
+    three_weeks = Time.now + (3*7*24*60*60) 
 
-    puts "SIMONE"
-    puts @old[-1].uid
+    if params[:query]
+      puts "SIMONE " << params[:query]
+      @future = Event.where("name LIKE ? and starts_at >= ?", "%#{params[:query]}%", three_weeks).limit(100).order('starts_at desc')
+      @upcoming = Event.where("name LIKE ? and starts_at >= ? and starts_at < ?", "%#{params[:query]}%", Time.zone.now.beginning_of_day, three_weeks).limit(100).order('starts_at desc')
+      @old = Event.where("name LIKE ? and starts_at < ?", "%#{params[:query]}%", Time.zone.now.beginning_of_day).limit(78).order('starts_at desc')
+    else
+      @future = Event.where("starts_at >= ?", three_weeks).limit(100).order('starts_at desc')
+      @upcoming = Event.where(:starts_at => Time.zone.now.beginning_of_day..three_weeks).limit(100).order('starts_at desc')
+      @old = Event.where("starts_at < ?", Time.zone.now.beginning_of_day).limit(78).order('starts_at desc')
+    end
+
     respond_to do |format|
-      events = {'upcoming' => @upcoming, 'future' => @future, 'old' => @old}
-      format.html { render :index }
-      format.json { render json: events }
+        events = {'upcoming' => @upcoming, 'future' => @future, 'old' => @old}
+        format.html { render :index }
+        format.json { render json: events }
     end
   end
 
