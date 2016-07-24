b7d8027b5906c5d2cc46cf815293ad2b2a75b630
sic2 <sic2@st-andrews.ac.uk>
2016-07-24 13:02:11 +0100

    home page : stats + links to pages

--- webapp/app/controllers/home_controller.rb
+++ webapp/app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
 
   def index
+    @posts_count = Post.where('created_at >= ?', 1.week.ago).count
+    @users_count = User.count
+    @communities_count = Page.communities.where('active = ?', true).count
+    @companies_count = Page.companies.where('active = ?', true).count
+
+    three_weeks = Time.now + (3*7*24*60*60) 
+    @upcoming_events_count = Event.where(:starts_at => Time.zone.now.beginning_of_day..three_weeks).limit(100).order('starts_at desc').count
   end
 
   def community_feed
