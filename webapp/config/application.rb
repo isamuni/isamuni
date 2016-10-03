71c3ec545e00836cae546dd794543e6e49138485
Gabriele Viglianisi <vigliag@gmail.com>
2016-10-03 19:55:57 +0200

    date internationalization in event table

--- webapp/config/application.rb
+++ webapp/config/application.rb
@@ -11,5 +11,10 @@ module Isamuni
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = "Rome"
+    I18n.available_locales = [:en, :it]
+    I18n.default_locale = :it
   end
 end
+
