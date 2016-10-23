409695098581c70b471a34eabb70bfb9d57336a7
Gabriele Viglianisi <vigliag@gmail.com>
2016-10-23 17:50:37 +0200

    removed deploy infos (moved in other repo). Enabled static file serving, made omniauth only initialize for rails server (not for rake tasks)

--- webapp/config/initializers/omniauth.rb
+++ webapp/config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
-raise "ISAMUNI_APP_ID not set" unless ENV['ISAMUNI_APP_ID']
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['ISAMUNI_APP_ID'], ENV['ISAMUNI_APP_SECRET']
+# Registers isamuni_app_id and isamuni_app_secret with omniauth
+# only if we are starting the server, so it doesn't run on rake tasks
+if defined?(Rails::Server) 
+	raise "ISAMUNI_APP_ID not set" unless ENV['ISAMUNI_APP_ID']
+	Rails.application.config.middleware.use OmniAuth::Builder do
+	  provider :facebook, ENV['ISAMUNI_APP_ID'], ENV['ISAMUNI_APP_SECRET']
+	end
 end
