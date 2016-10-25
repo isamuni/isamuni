4b62ba664014976411a0df63270ea426c6589424
Gabriele Viglianisi <vigliag@gmail.com>
2016-10-25 09:19:58 +0200

    now correctly serving minified assets. Loggin to stdout controlled by env variable

--- webapp/config/environments/production.rb
+++ webapp/config/environments/production.rb
@@ -42,9 +42,6 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Make rails log to STDOUT
-  config.logger = Logger.new(STDOUT)
-
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
