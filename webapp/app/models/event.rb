4c56bbc6f3377aa7b0b08da600e256969dd39742
sic2 <sic2@st-andrews.ac.uk>
2016-10-29 16:39:47 +0100

    fix minor bugs

--- webapp/app/models/event.rb
+++ webapp/app/models/event.rb
@@ -2,8 +2,8 @@ require_relative "sanitize"
 
 class Event < ApplicationRecord
 
-  scope :future, -> { where "starts_at >= ?", Time.zone.now.beginning_of_day }
-  scope :past, -> { where "starts_at < ?", Time.zone.now.beginning_of_day }
+  scope :future, -> { where "ends_at >= ?", Time.zone.now.beginning_of_day }
+  scope :past, -> { where "ends_at < ?", Time.zone.now.beginning_of_day }
   scope :only_with_coordinates, -> { where "coordinates IS NOT NULL" }
 
   def self.name_like query
@@ -43,7 +43,7 @@ class Event < ApplicationRecord
   end
 
   def as_json(options={})
-    super(only: [:name, :starts_at, :content, :location, :location_name])
+    super(only: [:name, :starts_at, :ends_at, :content, :location, :location_name])
   end
 
 end
