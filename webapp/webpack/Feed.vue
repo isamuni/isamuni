eca29b7b50e47164fea916529cd3304a2d5ce166
sic2 <sic2@st-andrews.ac.uk>
2016-11-21 12:58:32 +0000

    showing current events in orange background + code beautification...not much choice in having that guy running there..

--- webapp/webpack/Feed.vue
+++ webapp/webpack/Feed.vue
@@ -1,35 +1,28 @@
 <template>
 <div id="feedapp">
-  <div id="dashboard_div" class="row">
-    <div class="col-md-3">
-      <multiselect
-        :options="sources"
-        :multiple="true"
-        :close-on-select="true"
-        @input="updateFilter"
-        placeholder="Scegli fonti"
-        label="name" />
-        <p>
-          <label for="jobs_only">Solo annunci di lavoro</label> <input id="jobs_only" type="checkbox" value="false" v-model="filter.jobs_only"></input>
-        </p>
+    <div id="dashboard_div" class="row">
+        <div class="col-md-3">
+            <multiselect :options="sources" :multiple="true" :close-on-select="true" @input="updateFilter" placeholder="Scegli fonti" label="name" />
+            <p>
+                <label for="jobs_only">Solo annunci di lavoro</label> <input id="jobs_only" type="checkbox" value="false" v-model="filter.jobs_only"></input>
+            </p>
+        </div>
+        <div class="col-md-9">
+            <i id="spinner" class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+            <div class="row" style="display:none">
+                <div id="chart_div" data-intro="Questo grafico mostra tutti i post della community nel tempo."></div>
+            </div>
+            <div class="row">
+                <div id="filter_div" data-intro="Usa lo slider per trovare i post più vecchi."></div>
+            </div>
+        </div>
     </div>
-    <div class="col-md-9">
-      <i id="spinner" class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
-      <div class="row">
-        <div id="chart_div" data-intro="Questo grafico mostra tutti i post della community nel tempo."></div>
-      </div>
-      <div class="row">
-        <div id="filter_div" data-intro="Usa lo slider per trovare i post più vecchi."></div>
-      </div>
-    </div>
-  </div>
-  <hr>
-  <PostDisplay v-if="sources_ready" :filter="{}" :posts="posts"></PostDisplay>
+    <hr>
+    <PostDisplay v-if="sources_ready" :filter="{}" :posts="posts"></PostDisplay>
 </div>
 </template>
 
 <script>
-
 import PostDisplay from './PostDisplay.vue';
 import Multiselect from 'vue-multiselect';
 
@@ -42,65 +35,64 @@ import Multiselect from 'vue-multiselect';
 /* global $, console */
 
 var Feed = {
-  data: function() {
-    return {
-      sources: [],
-      filter: {
-        sources: "",
-        start: null,
-        end: null,
-        jobs_only: 0
-      },
-      posts: []
-    };
-  },
-  mounted: function() {
-    let sources = $.getJSON('/feed/sources.json');
-
-    sources.then((sources) => {
-      this.sources = sources;
-      this.updatePosts();
-    });
-  },
-  computed: {
-    sources_ready: function() {
-      return Object.keys(this.sources).length > 0;
-    }
-  },
-  watch: {
-    filter: {
-      handler: function() {
-        this.updatePosts();
-      },
-      deep: true
-    }
-  },
-  methods: {
-    updateFilter(selectedElems) {
-      let selectedIDs = selectedElems.map((e) => e.id);
-      selectedIDs.sort();
-      this.filter.sources = selectedIDs.join(",");
+    data: function() {
+        return {
+            sources: [],
+            filter: {
+                sources: "",
+                start: null,
+                end: null,
+                jobs_only: 0
+            },
+            posts: []
+        };
     },
-    updatePosts() {
-      let _this = this;
-      let result = $.getJSON('/feed/posts.json', this.filter);
+    mounted: function() {
+        let sources = $.getJSON('/feed/sources.json');
 
-      result.then(function(posts) {
-        posts.forEach(function(post) {
-          post['source'] = _this.sources.find((s) => s.id == post['source_id']) || {};
+        sources.then((sources) => {
+            this.sources = sources;
+            this.updatePosts();
         });
-        _this.posts = posts;
-      });
+    },
+    computed: {
+        sources_ready: function() {
+            return Object.keys(this.sources).length > 0;
+        }
+    },
+    watch: {
+        filter: {
+            handler: function() {
+                this.updatePosts();
+            },
+            deep: true
+        }
+    },
+    methods: {
+        updateFilter(selectedElems) {
+            let selectedIDs = selectedElems.map((e) => e.id);
+            selectedIDs.sort();
+            this.filter.sources = selectedIDs.join(",");
+        },
+        updatePosts() {
+            let _this = this;
+            let result = $.getJSON('/feed/posts.json', this.filter);
+
+            result.then(function(posts) {
+                posts.forEach(function(post) {
+                    post['source'] = _this.sources.find((s) => s.id == post['source_id']) || {};
+                });
+                _this.posts = posts;
+            });
+        }
+    },
+    components: {
+        PostDisplay,
+        Multiselect
     }
-  },
-  components: {
-    PostDisplay,
-    Multiselect
-  }
 };
 
 export default Feed;
-
 </script>
 
 <style>
