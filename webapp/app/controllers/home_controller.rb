cb30f75315a36e2a002d3a15aee8a4da4703b7ce
sic2 <sic2@st-andrews.ac.uk>
2017-09-04 15:06:12 +0100

    default icons for companies/communities + fixes small screen rendering issues

--- webapp/app/controllers/home_controller.rb
+++ webapp/app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
-    @posts_count = Post.where('created_at >= ?', 1.week.ago).count
+    @posts_count = Post.count
+    @posts_week_count = Post.where('created_at >= ?', 1.week.ago).count
     @users_count = User.count
     @communities_count = Page.communities.where('active = ?', true).count
     @companies_count = Page.companies.where('active = ?', true).count
-
+    @events_count = Event.count
     today = Time.zone.now.beginning_of_day
     @upcoming_events_count = Event.where('ends_at >= ? OR starts_at >= ?', today, today)
                                   .order('ends_at desc')
