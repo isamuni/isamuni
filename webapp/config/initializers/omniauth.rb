91e1941d4770444d78d80bc8846d8539c54775ab
sic2 <sic2@st-andrews.ac.uk>
2017-09-07 11:20:35 +0100

    ui changes

--- webapp/config/initializers/omniauth.rb
+++ webapp/config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Registers isamuni_app_id and isamuni_app_secret with omniauth
 # only if we are starting the server, so it doesn't run on rake tasks
 if defined?(Rails::Server)
