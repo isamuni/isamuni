4d95f3f41cbdf4ec2f08c8797e76633a940a5b80
Gabriele Viglianisi <vigliag@gmail.com>
2017-09-11 12:09:22 +0200

    easier docker development environment, and no mandatory env variable

--- database/init_db.sql
+++ database/init_db.sql
@@ -1,1 +1,1 @@
-create extension pg_trgm
+CREATE EXTENSION pg_trgm;
--- webapp/config/initializers/omniauth.rb
+++ webapp/config/initializers/omniauth.rb
@@ -3,8 +3,11 @@
 # Registers isamuni_app_id and isamuni_app_secret with omniauth
 # only if we are starting the server, so it doesn't run on rake tasks
 if defined?(Rails::Server)
-  raise 'ISAMUNI_APP_ID not set' unless ENV['ISAMUNI_APP_ID']
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['ISAMUNI_APP_ID'], ENV['ISAMUNI_APP_SECRET']
+  unless ENV['ISAMUNI_APP_ID']
+    Rails.logger.error "env variable ISAMUNI_APP_ID not set. Facebook-related functionalities will not work"
+  else
+    Rails.application.config.middleware.use OmniAuth::Builder do
+      provider :facebook, ENV['ISAMUNI_APP_ID'], ENV['ISAMUNI_APP_SECRET']
+    end
   end
 end
